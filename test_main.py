108282a60a98ea531c5b15b590068cb6419b8298
Jose Manzaneque <jmm0693@gmail.com>
2025-01-23 01:14:20 +0100

    Add palindrome endpoint and corresponding tests

--- test_main.py
+++ test_main.py
@@ -16,5 +16,20 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"disemvoweled": ""})
 
+    def test_palindrome_text_empty(self):
+        response = client.get("/api/palindrome?text=")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"palindrome": True})
+
+    def test_palindrome_text_true(self):
+        response = client.get("/api/palindrome?text=A man a plan a canal Panama")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"palindrome": True})
+
+    def test_palindrome_text_false(self):
+        response = client.get("/api/palindrome?text=Hello World")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"palindrome": False})
+
 if __name__ == '__main__':
     unittest.main()
